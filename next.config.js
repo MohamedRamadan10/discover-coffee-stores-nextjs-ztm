fe4e576d89b755ca25c37ffbc12449f632922f6a
Mohamed Ramadan <mr.uiux.dev@gmail.com>
2023-02-22 19:55:42 +0200

    config

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+	strictMode: false,
+	env: {
+		API_AIRTABLE_KEY: "keyisCUd82F9lIdF8",
+		BASE_AIRTABLE_KEY: "appp7UhNGyoMF078V",
+		API_FOURSQUARE_KEY: "fsq3pTaoDIjDkIRTTg1c4vzBlZmKAwJLAg602GEZUbOjUpE=",
+		API_UNSPLASH_KEY: "3zZ_FTkRA2dLQ_3Hgn4PI5PDUaMg4wPJXQnAHw0cWBM",
+	},
+	images: {
+		domains: ["images.unsplash.com"],
+	},
+};
+
+module.exports = nextConfig;
